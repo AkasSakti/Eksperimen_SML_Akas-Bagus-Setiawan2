bbbc51989a42bd1d0faad5c9c8e35114633beadd
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2025-05-22 15:40:35 +0700

    modelling_tuning.py

--- Membangun_model/modelling_tuning.py
+++ Membangun_model/modelling_tuning.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-df = pd.read_csv("D:/nang jember/Akas Bagus Setiawan/DICODING/Eksperimen_SML_Akas-Bagus-Setiawan2/preprocessing/olshopdatapreprocesed/online_shoppers_intention_preprocessed.csv")
+df = pd.read_csv("Eksperimen_SML_Akas-Bagus-Setiawan2/preprocessing/olshopdatapreprocesed/online_shoppers_intention_preprocessed.csv")
 X = df.drop("Revenue", axis=1)
 y = df["Revenue"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
